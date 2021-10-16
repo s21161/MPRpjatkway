d06cf5cf4e47705dfb35750fa0dac66107f0733f
Artur <s21161@pjwstk.edu.pl>
2021-10-16 12:21:08 +0200

    mpr

---
+++ src/main/java/pl/pjwstk/pjatkway/PjatkwayApplication.java
@@ -0,0 +1,13 @@
+package pl.pjwstk.pjatkway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PjatkwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PjatkwayApplication.class, args);
+	}
+
+}
